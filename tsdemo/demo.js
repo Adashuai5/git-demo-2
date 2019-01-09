03877bb96c0f9225486b38f5b3b181572dde4da8
adashuai5 <ad873606339@gmail.com>
2019-01-10 00:32:42 +0800

    TS 里写排序和 js 一致

--- tsdemo/demo.js
+++ tsdemo/demo.js
@@ -1,3 +1,19 @@
+function selectSort(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        var minIndex = i;
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+        var temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+var Arr = selectSort([12, 31245, 354, 2, 41, 21]);
+console.log(Arr);
 function min(a, b) {
     if (a < b) {
         return a;
--- tsdemo/demo.ts
+++ tsdemo/demo.ts
@@ -1,3 +1,21 @@
+function selectSort(arr: number[]): number[] {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+        let temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+let Arr = selectSort([12,31245,354,2,41,21]);
+console.log(Arr);
+
 function min(a: number, b: number): number {
     if (a < b) {
         return a;
