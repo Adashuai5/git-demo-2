3a8aa91ef7f7c2f7a7de196a68ec7be0fcb4f949
adashuai5 <ad873606339@gmail.com>
2020-01-05 15:42:11 +0800

    完整 EventHub 代码及测试

--- eventhub/Test/index.ts
+++ eventhub/Test/index.ts
@@ -1,14 +1,40 @@
 import EventHub from '../src/index'
 
-const eventHub = new EventHub()
+let test1 = (message) => {
+  const eventHub = new EventHub()
+  console.assert(eventHub instanceof Object === true, 'eventHub 是对象')
+  console.log(message)
+}
 
-console.assert(eventHub instanceof Object === true, 'eventHub 是对象')
+let test2 = (message) => {
+  const eventHub = new EventHub()
+  let called = false
+  eventHub.on('xxx', y => {
+    called = true
+    console.assert(y === '我真帅！')
+  })
+  eventHub.emit('xxx', '我真帅！')
+  setTimeout(() => {
+    console.log(message)
+    console.assert(called === true)
+  }, 1000)
+}
 
-let called = false
+let test3 = (message) => {
+  const eventHub = new EventHub()
+  let called = false
+  const fn1 = () => {
+    called = true
+  }
+  eventHub.on('xxx', fn1)
+  eventHub.off('xxx', fn1)
+  eventHub.emit('xxx')
+  setTimeout(() => {
+    console.log(message)
+    console.assert(called === false)
+  }, 1000)
+}
 
-eventHub.on('xxx', () => {
-  called = true
-  console.log('called：' + called)
-})
-
-eventHub.emit('xxx')
+test1('EventHub 可以创建对象')
+test2('.on 之后，.emit 会触发 .on 的函数')
+test3('.off 有用')
--- eventhub/src/index.ts
+++ eventhub/src/index.ts
@@ -1,12 +1,36 @@
 class EventHub {
-  catch = {}
+  private catch = {}
   on(eventName, fn) {
     this.catch[eventName] = this.catch[eventName] || []
     this.catch[eventName].push(fn)
   }
-  emit(eventName) {
-    ;(this.catch[eventName] || []).forEach(fn => fn())
+  emit(eventName, data?) {
+    ;(this.catch[eventName] || []).forEach(fn => fn(data))
+  }
+  off(eventName, fn) {
+    let index = this.catch[eventName].indexOf(fn)
+    if (index === -1) return
+    this.catch[eventName].splice(index, 1)
   }
 }
 
 export default EventHub
+
+/**
+ * 帮助函数 indexOf
+ * @param array
+ * @param item
+ */
+
+function indexOf(array, item) {
+  if (array === undefined) {
+    return -1
+  }
+  let index = -1
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      index = i
+    }
+  }
+  return index
+}
