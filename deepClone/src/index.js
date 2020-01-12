9b1f83b7ddbaa64bc17d016bd720b00af424b987
adashuai5 <ad873606339@gmail.com>
2020-01-12 19:14:15 +0800

    格式化一下代码

--- deepClone/src/index.js
+++ deepClone/src/index.js
@@ -1,44 +1,45 @@
-let cache = []
+let cache = [];
 function deepClone(source) {
   if (source instanceof Object) {
-    let cachedDist = findCache(source)
+    let cachedDist = findCache(source);
     if (cachedDist) {
-      return cachedDist
+      return cachedDist;
     } else {
-      let dist
+      let dist;
       if (source instanceof Array) {
-        dist = new Array()
+        dist = new Array();
       } else if (source instanceof Function) {
         // 通过调用自己本身获取 this 和 参数
-        dist = function () {
-          return source.apply(this, arguments)
-        }
+        dist = function() {
+          return source.apply(this, arguments);
+        };
       } else if (source instanceof RegExp) {
-        dist = new RegExp(source.source, source.flags)
+        dist = new RegExp(source.source, source.flags);
       } else if (source instanceof Date) {
-        dist = new Date(source)
+        dist = new Date(source);
       } else {
-        dist = new Object()
+        dist = new Object();
       }
-      cache.push([source, dist])
+      cache.push([source, dist]);
       // 通过 hasOwnProperty 判断 key 是否为 source 自身属性
-      for (let key in source) if (source.hasOwnProperty(key)) {
-        dist[key] = deepClone(source[key])
-      }
-      return dist
+      for (let key in source)
+        if (source.hasOwnProperty(key)) {
+          dist[key] = deepClone(source[key]);
+        }
+      return dist;
     }
   }
-  return source
+  return source;
 }
 
 function findCache(source) {
   for (let i = 0; i < cache.length; i++) {
     if (cache[i][0] === source) {
-      return cache[i][1]
+      return cache[i][1];
     }
   }
-  return undefined
+  return undefined;
 }
 
-module.exports = deepClone
+module.exports = deepClone;
 //
--- deepClone/test/index.js
+++ deepClone/test/index.js
@@ -1,120 +1,120 @@
-const chai = require('chai')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-chai.use(sinonChai)
+const chai = require("chai");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai);
 
-const assert = chai.assert
-const deepClone = require('../src/index')
+const assert = chai.assert;
+const deepClone = require("../src/index");
 
-describe('deepClone', () => {
-  it('是一个函数', () => {
-    assert.isFunction(deepClone)
-  })
-  it('能够复制基本类型', () => {
-    const n = 123
-    const n1 = deepClone(n)
-    assert.equal(n, n1)
-    const s = '123'
-    const s1 = deepClone(s)
-    assert.equal(n, s1)
-    const b = true
-    const b1 = deepClone(b)
-    assert.equal(b, b1)
-    const u = undefined
-    const u1 = deepClone(u)
-    assert.equal(u, u1)
-    const empty = null
-    const empty1 = deepClone(empty)
-    assert.equal(empty, empty1)
+describe("deepClone", () => {
+  it("是一个函数", () => {
+    assert.isFunction(deepClone);
+  });
+  it("能够复制基本类型", () => {
+    const n = 123;
+    const n1 = deepClone(n);
+    assert.equal(n, n1);
+    const s = "123";
+    const s1 = deepClone(s);
+    assert.equal(n, s1);
+    const b = true;
+    const b1 = deepClone(b);
+    assert.equal(b, b1);
+    const u = undefined;
+    const u1 = deepClone(u);
+    assert.equal(u, u1);
+    const empty = null;
+    const empty1 = deepClone(empty);
+    assert.equal(empty, empty1);
     // Symbol 不细究了
-    const symbol = Symbol()
-    const symbol1 = deepClone(symbol)
-    assert(symbol === symbol1)
-  })
-  describe('对象', () => {
-    it('能够复制对象', () => {
-      const a = { name: 'ada', child: { name: 'little ada' } }
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert(a.name === a1.name)
-      assert(a.child !== a1.child)
-      assert(a.child.name === a1.child.name)
-    })
-    it('能够复制数组', () => {
+    const symbol = Symbol();
+    const symbol1 = deepClone(symbol);
+    assert(symbol === symbol1);
+  });
+  describe("对象", () => {
+    it("能够复制对象", () => {
+      const a = { name: "ada", child: { name: "little ada" } };
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert(a.name === a1.name);
+      assert(a.child !== a1.child);
+      assert(a.child.name === a1.child.name);
+    });
+    it("能够复制数组", () => {
       const a = [
         [1, 2, 3],
         [2, 3, 4],
         [12, 23, 34]
-      ]
-      const a1 = deepClone(a)
-      assert(a[0] !== a1[0])
-      assert(a[1] !== a1[1])
-      assert(a[2] !== a1[2])
-      assert.deepEqual(a, a1)
-    })
-    it('能够复制函数', () => {
-      const a = function () {
-        return 1
-      }
-      a.xxx = { yyy: { zzz: 1 } }
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz)
-      assert(a.xxx.yyy !== a1.xxx.yyy)
-      assert(a.xxx !== a1.xxx)
-      assert(a() === a1())
-    })
-    it('能够复制环', () => {
-      const a = { name: 'ada', child: { name: 'little ada' } }
-      a.self = a
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert(a.name === a1.name)
-      assert(a.self !== a1.self)
-    })
-    // xit 表示不测试，我们不考虑爆栈的情况
-    xit('不会爆栈', () => {
-      const a = { child: null }
-      let b = a
-      for (let i = 0; i < 5458; i++) {
-        b.child = { child: null }
-        b = b.child
-      }
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert(a.child !== a1.child)
-    })
-    it('能够复制正则', () => {
-      // new RegExp(source, flags)
-      const a = new RegExp('hi\\d+', 'gi')
-      a.xxx = { yyy: { zzz: 1 } }
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert(a.source === a1.source)
-      assert(a.flags === a1.flags)
-      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz)
-      assert(a.xxx.yyy !== a1.xxx.yyy)
-      assert(a.xxx !== a1.xxx)
-    })
-    it('能够复制日期', () => {
-      const a = new Date()
-      a.xxx = { yyy: { zzz: 1 } }
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert(a.getTime() === a1.getTime())
-      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz)
-      assert(a.xxx.yyy !== a1.xxx.yyy)
-      assert(a.xxx !== a1.xxx)
-    })
-    it("能够跳过原型", () => {
-      const a = Object.create({ name: "a" })
-      a.xxx = { yyy: { zzz: 1 } }
-      const a1 = deepClone(a)
-      assert(a !== a1)
-      assert.isFalse("name" in a1)
-      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz)
-      assert(a.xxx.yyy !== a1.xxx.yyy)
-      assert(a.xxx !== a1.xxx)
+      ];
+      const a1 = deepClone(a);
+      assert(a[0] !== a1[0]);
+      assert(a[1] !== a1[1]);
+      assert(a[2] !== a1[2]);
+      assert.deepEqual(a, a1);
     });
-  })
-})
+    it("能够复制函数", () => {
+      const a = function() {
+        return 1;
+      };
+      a.xxx = { yyy: { zzz: 1 } };
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz);
+      assert(a.xxx.yyy !== a1.xxx.yyy);
+      assert(a.xxx !== a1.xxx);
+      assert(a() === a1());
+    });
+    it("能够复制环", () => {
+      const a = { name: "ada", child: { name: "little ada" } };
+      a.self = a;
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert(a.name === a1.name);
+      assert(a.self !== a1.self);
+    });
+    // xit 表示不测试，我们不考虑爆栈的情况
+    xit("不会爆栈", () => {
+      const a = { child: null };
+      let b = a;
+      for (let i = 0; i < 5458; i++) {
+        b.child = { child: null };
+        b = b.child;
+      }
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert(a.child !== a1.child);
+    });
+    it("能够复制正则", () => {
+      // new RegExp(source, flags)
+      const a = new RegExp("hi\\d+", "gi");
+      a.xxx = { yyy: { zzz: 1 } };
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert(a.source === a1.source);
+      assert(a.flags === a1.flags);
+      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz);
+      assert(a.xxx.yyy !== a1.xxx.yyy);
+      assert(a.xxx !== a1.xxx);
+    });
+    it("能够复制日期", () => {
+      const a = new Date();
+      a.xxx = { yyy: { zzz: 1 } };
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert(a.getTime() === a1.getTime());
+      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz);
+      assert(a.xxx.yyy !== a1.xxx.yyy);
+      assert(a.xxx !== a1.xxx);
+    });
+    it("能够跳过原型", () => {
+      const a = Object.create({ name: "a" });
+      a.xxx = { yyy: { zzz: 1 } };
+      const a1 = deepClone(a);
+      assert(a !== a1);
+      assert.isFalse("name" in a1);
+      assert(a.xxx.yyy.zzz === a1.xxx.yyy.zzz);
+      assert(a.xxx.yyy !== a1.xxx.yyy);
+      assert(a.xxx !== a1.xxx);
+    });
+  });
+});
