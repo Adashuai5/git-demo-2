0c2b70791f828cfc5a996395ba3d33e25f47a3e5
adashuai5 <ad873606339@gmail.com>
2020-01-16 22:05:20 +0800

    ES3 版本的 polyfill

--- bind/src/index.js
+++ bind/src/index.js
@@ -1,13 +1,28 @@
-function bind(callThis, ...args) {
-  const fn = this
-  return function () {
-    return fn.call(callThis, ...args)
-  }
+// ES6
+
+function _bind(callThis, ...args) {
+  const fn = this;
+  return function() {
+    return fn.call(callThis, ...args);
+  };
 }
 
-module.exports = bind
+// ES3
+var slice = Array.prototype.slice;
+function bind(callThis) {
+  var fn = this;
+  if (typeof fn !== "function") {
+    throw new Error("bind 必须绑定在函数身上");
+  }
+  var args = slice.call(arguments, 1);
+  return function() {
+    var args2 = slice.call(arguments, 0);
+    return fn.apply(callThis, args.concat(args2));
+  };
+}
 
+module.exports = bind;
 
 if (!Function.prototype.bind) {
-  Function.prototype.bind = bind
+  Function.prototype.bind = bind;
 }
--- bind/test/index.js
+++ bind/test/index.js
@@ -1,33 +1,21 @@
-const bind = require('../src/index');
+const bind = require("../src/index");
 
-Function.prototype.bind2 = bind
-console.assert(
-  Function.prototype.bind !== undefined
-)
-console.assert(
-  Function.prototype.bind2 !== undefined
-)
+Function.prototype.bind2 = bind;
+console.assert(Function.prototype.bind !== undefined);
+console.assert(Function.prototype.bind2 !== undefined);
 
-const fn = function () {
-  return this
-}
+const fn = function() {
+  return this;
+};
 
-console.assert(
-  fn.bind2({ name: 'ada' })().name === 'ada'
-)
+console.assert(fn.bind2({ name: "ada" })().name === "ada");
 
-const fn2 = function (p1, p2) {
+const fn2 = function(p1, p2) {
   return [this, p1, p2];
 };
 
-let newFn = fn2.bind2({ name: 'ada' }, 111, 222)
+let newFn = fn2.bind2({ name: "ada" }, 111, 222);
 
-console.assert(
-  newFn()[0].name === 'ada', 'this'
-)
-console.assert(
-  newFn()[1] === 111, '111'
-)
-console.assert(
-  newFn()[2] === 222, '222'
-)
+console.assert(newFn()[0].name === "ada", "this");
+console.assert(newFn()[1] === 111, "111");
+console.assert(newFn()[2] === 222, "222");
