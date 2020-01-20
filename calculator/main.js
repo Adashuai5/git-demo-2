998d0749bee8563f8d64d4c33cf5d238b656f311
yd.zhou <yd.zhou@foxmail.com>
2020-01-20 15:54:13 +0800

    用 class 优化代码

--- calculator/main.js
+++ calculator/main.js
@@ -1,76 +1,99 @@
 {
-    function createButton(text, container, className) {
-        var button = document.createElement("button");
-        button.textContent = text;
-        if (className) {
-            button.className = className;
+    var Calculator = /** @class */ (function () {
+        function Calculator() {
+            this.createContainer();
+            this.createOutput();
+            this.createKeys();
+            this.eventHub();
         }
-        container.appendChild(button);
-    }
-    var container_1 = document.createElement("div");
-    container_1.classList.add("calculator");
-    document.body.appendChild(container_1);
-    var output = document.createElement("div");
-    output.classList.add("output");
-    container_1.appendChild(output);
-    var span_1 = document.createElement("span");
-    span_1.textContent = "0";
-    output.appendChild(span_1);
-    var n1_1, operation_1, n2_1;
-    container_1.addEventListener("click", function (event) {
-        if (event.target instanceof HTMLButtonElement) {
-            var buttonText = event.target.textContent;
-            if ("0123456789".indexOf(buttonText) >= 0) {
-                if (operation_1) {
-                    n2_1 = n2_1 ? parseInt(n2_1 + buttonText) : parseInt(buttonText);
-                    span_1.textContent = n2_1.toString();
-                }
-                else {
-                    n1_1 = n1_1 ? parseInt(n1_1 + buttonText) : parseInt(buttonText);
-                    span_1.textContent = n1_1.toString();
-                }
+        Calculator.prototype.createButton = function (text, container, className) {
+            var button = document.createElement("button");
+            button.textContent = text;
+            if (className) {
+                button.className = className;
             }
-            else if ("+-×÷".indexOf(buttonText) >= 0) {
-                operation_1 = buttonText;
-            }
-            else if ("=".indexOf(buttonText) >= 0) {
-                if (operation_1 === "+") {
-                    span_1.textContent = (n1_1 + n2_1).toString();
+            container.appendChild(button);
+        };
+        Calculator.prototype.createContainer = function () {
+            this.container = document.createElement("div");
+            this.container.classList.add("calculator");
+            document.body.appendChild(this.container);
+        };
+        Calculator.prototype.createOutput = function () {
+            this.output = document.createElement("div");
+            this.output.classList.add("output");
+            this.container.appendChild(this.output);
+            this.span = document.createElement("span");
+            this.span.textContent = "0";
+            this.output.appendChild(this.span);
+        };
+        Calculator.prototype.createKeys = function () {
+            var _this = this;
+            var keys = [
+                ["clear", "÷"],
+                ["7", "8", "9", "×"],
+                ["4", "5", "6", "-"],
+                ["1", "2", "3", "+"],
+                ["0", ".", "="]
+            ];
+            keys.forEach(function (textList) {
+                var div = document.createElement("div");
+                div.classList.add("row");
+                textList.forEach(function (text) {
+                    _this.createButton(text, div, "button text-" + text);
+                });
+                _this.container.appendChild(div);
+            });
+        };
+        Calculator.prototype.eventHub = function () {
+            var _this = this;
+            this.container.addEventListener("click", function (event) {
+                if (event.target instanceof HTMLButtonElement) {
+                    var buttonText = event.target.textContent;
+                    if ("0123456789".indexOf(buttonText) >= 0) {
+                        if (_this.operation) {
+                            _this.n2 = _this.n2
+                                ? parseInt(_this.n2 + buttonText)
+                                : parseInt(buttonText);
+                            _this.span.textContent = _this.n2.toString();
+                        }
+                        else {
+                            _this.n1 = _this.n1
+                                ? parseInt(_this.n1 + buttonText)
+                                : parseInt(buttonText);
+                            _this.span.textContent = _this.n1.toString();
+                        }
+                    }
+                    else if ("+-×÷".indexOf(buttonText) >= 0) {
+                        _this.operation = buttonText;
+                    }
+                    else if ("=".indexOf(buttonText) >= 0) {
+                        if (_this.operation === "+") {
+                            _this.span.textContent = (_this.n1 + _this.n2).toString();
+                        }
+                        else if (_this.operation === "-") {
+                            _this.span.textContent = (_this.n1 - _this.n2).toString();
+                        }
+                        else if (_this.operation === "×") {
+                            _this.span.textContent = (_this.n1 * _this.n2).toString();
+                        }
+                        else if (_this.operation === "÷") {
+                            _this.span.textContent = (_this.n1 / _this.n2).toFixed(1).toString();
+                        }
+                        _this.n1 = 0;
+                        _this.n2 = 0;
+                        _this.operation = "";
+                    }
+                    else if (buttonText === "clear") {
+                        _this.span.textContent = "0";
+                        _this.n1 = 0;
+                        _this.n2 = 0;
+                        _this.operation = "";
+                    }
                 }
-                else if (operation_1 === "-") {
-                    span_1.textContent = (n1_1 - n2_1).toString();
-                }
-                else if (operation_1 === "×") {
-                    span_1.textContent = (n1_1 * n2_1).toString();
-                }
-                else if (operation_1 === "÷") {
-                    span_1.textContent = (n1_1 / n2_1).toFixed(1).toString();
-                }
-                n1_1 = 0;
-                n2_1 = 0;
-                operation_1 = "";
-            }
-            else if (buttonText === "clear") {
-                span_1.textContent = "0";
-                n1_1 = 0;
-                n2_1 = 0;
-                operation_1 = "";
-            }
-        }
-    });
-    var keys = [
-        ["clear", "÷"],
-        ["7", "8", "9", "×"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", "="]
-    ];
-    keys.forEach(function (textList) {
-        var div = document.createElement("div");
-        div.classList.add("row");
-        textList.forEach(function (text) {
-            createButton(text, div, "button text-" + text);
-        });
-        container_1.appendChild(div);
-    });
+            });
+        };
+        return Calculator;
+    }());
+    new Calculator();
 }
--- calculator/main.ts
+++ calculator/main.ts
@@ -1,79 +1,107 @@
 {
-  function createButton(
-    text: string,
-    container: HTMLElement,
-    className: string
-  ): void {
-    let button: HTMLButtonElement = document.createElement("button");
-    button.textContent = text;
-    if (className) {
-      button.className = className;
+  class Calculator {
+    public container: HTMLDivElement;
+    private output: HTMLDivElement;
+    private span: HTMLSpanElement;
+    public n1: number;
+    public operation: string;
+    public n2: number;
+
+    constructor() {
+      this.createContainer();
+      this.createOutput();
+      this.createKeys();
+      this.eventHub();
     }
-    container.appendChild(button);
+
+    createButton(
+      text: string,
+      container: HTMLElement,
+      className: string
+    ): void {
+      let button: HTMLButtonElement = document.createElement("button");
+      button.textContent = text;
+      if (className) {
+        button.className = className;
+      }
+      container.appendChild(button);
+    }
+
+    createContainer() {
+      this.container = document.createElement("div");
+      this.container.classList.add("calculator");
+      document.body.appendChild(this.container);
+    }
+
+    createOutput() {
+      this.output = document.createElement("div");
+      this.output.classList.add("output");
+      this.container.appendChild(this.output);
+      this.span = document.createElement("span");
+      this.span.textContent = "0";
+      this.output.appendChild(this.span);
+    }
+
+    createKeys() {
+      let keys: Array<Array<string>> = [
+        ["clear", "÷"],
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "="]
+      ];
+      keys.forEach((textList: Array<string>) => {
+        let div: HTMLDivElement = document.createElement("div");
+        div.classList.add("row");
+        textList.forEach((text: string) => {
+          this.createButton(text, div, `button text-${text}`);
+        });
+        this.container.appendChild(div);
+      });
+    }
+
+    eventHub() {
+      this.container.addEventListener("click", event => {
+        if (event.target instanceof HTMLButtonElement) {
+          let buttonText = event.target.textContent;
+
+          if ("0123456789".indexOf(buttonText) >= 0) {
+            if (this.operation) {
+              this.n2 = this.n2
+                ? parseInt(this.n2 + buttonText)
+                : parseInt(buttonText);
+              this.span.textContent = this.n2.toString();
+            } else {
+              this.n1 = this.n1
+                ? parseInt(this.n1 + buttonText)
+                : parseInt(buttonText);
+              this.span.textContent = this.n1.toString();
+            }
+          } else if ("+-×÷".indexOf(buttonText) >= 0) {
+            this.operation = buttonText;
+          } else if ("=".indexOf(buttonText) >= 0) {
+            if (this.operation === "+") {
+              this.span.textContent = (this.n1 + this.n2).toString();
+            } else if (this.operation === "-") {
+              this.span.textContent = (this.n1 - this.n2).toString();
+            } else if (this.operation === "×") {
+              this.span.textContent = (this.n1 * this.n2).toString();
+            } else if (this.operation === "÷") {
+              this.span.textContent = (this.n1 / this.n2).toFixed(1).toString();
+            }
+            this.n1 = 0;
+            this.n2 = 0;
+            this.operation = "";
+          } else if (buttonText === "clear") {
+            this.span.textContent = "0";
+            this.n1 = 0;
+            this.n2 = 0;
+            this.operation = "";
+          }
+        }
+      });
+    }
   }
 
-  let container: HTMLDivElement = document.createElement("div");
-  container.classList.add("calculator");
-  document.body.appendChild(container);
-
-  let output: HTMLDivElement = document.createElement("div");
-  output.classList.add("output");
-  container.appendChild(output);
-
-  let span: HTMLSpanElement = document.createElement("span");
-  span.textContent = "0";
-  output.appendChild(span);
-
-  let n1: number, operation: string, n2: number;
-  container.addEventListener("click", event => {
-    if (event.target instanceof HTMLButtonElement) {
-      let buttonText = event.target.textContent;
-
-      if ("0123456789".indexOf(buttonText) >= 0) {
-        if (operation) {
-          n2 = n2 ? parseInt(n2 + buttonText) : parseInt(buttonText);
-          span.textContent = n2.toString();
-        } else {
-          n1 = n1 ? parseInt(n1 + buttonText) : parseInt(buttonText);
-          span.textContent = n1.toString();
-        }
-      } else if ("+-×÷".indexOf(buttonText) >= 0) {
-        operation = buttonText;
-      } else if ("=".indexOf(buttonText) >= 0) {
-        if (operation === "+") {
-          span.textContent = (n1 + n2).toString();
-        } else if (operation === "-") {
-          span.textContent = (n1 - n2).toString();
-        } else if (operation === "×") {
-          span.textContent = (n1 * n2).toString();
-        } else if (operation === "÷") {
-          span.textContent = (n1 / n2).toFixed(1).toString();
-        }
-        n1 = 0;
-        n2 = 0;
-        operation = "";
-      } else if (buttonText === "clear") {
-        span.textContent = "0";
-        n1 = 0;
-        n2 = 0;
-        operation = "";
-      }
-    }
-  });
-
-  let keys: Array<Array<string>> = [
-    ["clear", "÷"],
-    ["7", "8", "9", "×"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="]
-  ];
-  keys.forEach((textList: Array<string>) => {
-    let div: HTMLDivElement = document.createElement("div");
-    div.classList.add("row");
-    textList.forEach((text: string) => {
-      createButton(text, div, `button text-${text}`);
-    });
-    container.appendChild(div);
-  });
+  new Calculator();
 }
