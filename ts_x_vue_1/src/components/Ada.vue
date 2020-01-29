880ef2d08fa0c9e1e5655d9fbe173e429bdea46e
adashuai5 <ad873606339@gmail.com>
2020-01-29 14:36:36 +0800

    vue-cli with ts

---
+++ ts_x_vue_1/src/components/Ada.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    I am Ada!
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Ada extends Vue {}
+</script>
