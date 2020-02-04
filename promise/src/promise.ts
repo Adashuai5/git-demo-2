6f4e18cf3bad6e161d6c67ff22e893eb692419d3
adashuai5 <ad873606339@gmail.com>
2020-02-05 00:03:29 +0800

    完善 Promise/A+ 测试用例（then 的 return）

--- promise/src/promise.ts
+++ promise/src/promise.ts
@@ -1,25 +1,28 @@
 class Promise2 {
   callback = [];
   state = "pending";
-  resolve(result) {
+  private resolveOrReject(state, data, i) {
     if (this.state !== "pending") return;
-    this.state = "fulfilled";
-    setTimeout(() => {
+    this.state = state;
+    nextTick(() => {
       this.callback.forEach(handler => {
-        if (typeof handler[0] === "function")
-          handler[0].call(undefined, result);
+        if (typeof handler[i] === "function") {
+          let x;
+          try {
+            x = handler[i].call(undefined, data);
+          } catch (e) {
+            return handler[2].reject(e);
+          }
+          handler[2].resolveWith(x);
+        }
       });
-    }, 0);
+    });
+  }
+  resolve(result) {
+    this.resolveOrReject("fulfilled", result, 0);
   }
   reject(reason) {
-    if (this.state !== "pending") return;
-    this.state = "rejected";
-    setTimeout(() => {
-      this.callback.forEach(handler => {
-        if (typeof handler[1] === "function")
-          handler[1].call(undefined, reason);
-      });
-    }, 0);
+    this.resolveOrReject("rejected", reason, 1);
   }
   constructor(fn) {
     if (typeof fn !== "function") {
@@ -35,8 +38,86 @@ class Promise2 {
     if (fail) {
       handler[1] = fail;
     }
+    handler[2] = new Promise2(() => {});
     this.callback.push(handler);
+    return handler[2];
+  }
+
+  resolveWith(x) {
+    if (this === x) {
+      this.resolveWithSelf();
+    } else if (x instanceof Promise2) {
+      this.resolveWithPromise(x);
+    } else if (x instanceof Object) {
+      this.resolveWithObject(x);
+    } else {
+      this.resolve(x);
+    }
+  }
+
+  private resolveWithSelf() {
+    this.reject(new TypeError());
+  }
+  private resolveWithPromise(x) {
+    x.then(
+      result => {
+        this.resolve(result);
+      },
+      reason => {
+        this.reject(reason);
+      }
+    );
+  }
+  private resolveWithObject(x) {
+    let then = this.getThen(x);
+    if (then instanceof Function) {
+      this.resolveWithThenable(x);
+    } else {
+      this.resolve(x);
+    }
+  }
+  private getThen(x) {
+    let then;
+    try {
+      then = x.then;
+    } catch (e) {
+      return this.reject(e);
+    }
+    return then;
+  }
+  private resolveWithThenable(x) {
+    try {
+      x.then(
+        y => {
+          this.resolveWith(y);
+        },
+        r => {
+          this.reject(r);
+        }
+      );
+    } catch (e) {
+      this.reject(e);
+    }
   }
 }
 
 export default Promise2;
+
+function nextTick(fn) {
+  // node.js 环境
+  if (process !== undefined && typeof process.nextTick === "function") {
+    return process.nextTick(fn);
+  } else {
+    // 浏览器环境
+    var counter = 1;
+    var observer = new MutationObserver(fn);
+    var textNode = document.createTextNode(String(counter));
+
+    observer.observe(textNode, {
+      characterData: true
+    });
+
+    counter = counter + 1;
+    textNode.data = String(counter);
+  }
+}
--- promise/test/index.ts
+++ promise/test/index.ts
@@ -155,7 +155,7 @@ describe("Chai 能用", () => {
       done();
     });
   });
-  it("2.2.6.2 then可以在同一个promise里被多次调用", done => {
+  it("2.2.6.2 then 可以在同一个 promise 里被多次调用", done => {
     const promise = new Promise1((resolve, reject) => {
       reject();
     });
@@ -169,6 +169,121 @@ describe("Chai 能用", () => {
       assert(callbacks[2].called);
       assert(callbacks[1].calledAfter(callbacks[0]));
       assert(callbacks[2].calledAfter(callbacks[1]));
+      done();
+    });
+  });
+  it("2.2.7 then 必须返回一个 promise", () => {
+    const promise = new Promise1(resolve => {
+      resolve();
+    });
+    const promise2 = promise.then(
+      () => {},
+      () => {}
+    );
+    // @ts-ignore
+    assert(promise2 instanceof Promise1);
+  });
+  it("2.2.7.1 如果 then(success, fail) 中的 success 返回一个值 x, 运行 [[Resolve]](promise2, x) ", done => {
+    const promise1 = new Promise1(resolve => {
+      resolve();
+    });
+    promise1
+      .then(
+        () => "成功",
+        () => {}
+      )
+      .then(result => {
+        assert.equal(result, "成功");
+        done();
+      });
+  });
+  it("2.2.7.1.2 success 的返回值是一个 Promise 实例", done => {
+    const promise1 = new Promise1(resolve => {
+      resolve();
+    });
+    const fn = sinon.fake();
+    const promise2 = promise1.then(
+      /*s1 */ () => new Promise1(resolve => resolve())
+    );
+    promise2.then(fn);
+    setTimeout(() => {
+      assert(fn.called);
+      done();
+    });
+  });
+  it("2.2.7.1.2 success 的返回值是一个 Promise 实例，且失败了", done => {
+    const promise1 = new Promise1(resolve => {
+      resolve();
+    });
+    const fn = sinon.fake();
+    const promise2 = promise1.then(
+      /*s1 */ () => new Promise1((resolve, reject) => reject())
+    );
+    promise2.then(null, fn);
+    setTimeout(() => {
+      assert(fn.called);
+      done();
+    });
+  });
+  it("2.2.7.1.2 fail 的返回值是一个 Promise 实例", done => {
+    const promise1 = new Promise1((resolve, reject) => {
+      reject();
+    });
+    const fn = sinon.fake();
+    const promise2 = promise1.then(
+      null,
+      () => new Promise1(resolve => resolve())
+    );
+    promise2.then(fn, null);
+    setTimeout(() => {
+      assert(fn.called);
+      done();
+    });
+  });
+  it("2.2.7.1.2 fail 的返回值是一个 Promise 实例，且失败了", done => {
+    const promise1 = new Promise1((resolve, reject) => {
+      reject();
+    });
+    const fn = sinon.fake();
+    const promise2 = promise1.then(
+      null,
+      () => new Promise1((resolve, reject) => reject())
+    );
+    promise2.then(null, fn);
+    setTimeout(() => {
+      assert(fn.called);
+      done();
+    });
+  });
+  it("2.2.7.2 如果 success 抛出一个异常 e, promise2 必须被拒绝", done => {
+    const promise1 = new Promise1((resolve, reject) => {
+      resolve();
+    });
+    const fn = sinon.fake();
+    const error = new Error();
+    const promise2 = promise1.then(() => {
+      throw error;
+    });
+    promise2.then(null, fn);
+    setTimeout(() => {
+      assert(fn.called);
+      assert(fn.calledWith(error));
+      done();
+    });
+  });
+  it("2.2.7.2 如果 fail 抛出一个异常 e, promise2 必须被拒绝", done => {
+    const promise1 = new Promise1((resolve, reject) => {
+      reject();
+    });
+    const fn = sinon.fake();
+    const error = new Error();
+    const promise2 = promise1.then(null, () => {
+      throw error;
+    });
+    promise2.then(null, fn);
+    setTimeout(() => {
+      assert(fn.called);
+      assert(fn.calledWith(error));
       done();
     });
   });
